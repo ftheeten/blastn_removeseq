9ddca2734b22c64d472aa532331b4923590e71ab
Franck Theeten <franck.theeten@africamuseum.be>
2022-07-21 23:11:55 +0200

    Add files via upload

---
+++ blast_check_and_remove.sh
@@ -0,0 +1,100 @@
+#!/bin/bash 
+
+perc_identity=80
+word_size=11
+declare -A blast_pattern_assoc
+
+while getopts ":o:s:q:p:w:" opt; do
+  case $opt in
+    o) output="$OPTARG"
+    ;;
+    q) query="$OPTARG"
+	 ;;
+    s) subject="$OPTARG"
+    ;;
+	p) perc_identity=${OPTARG}
+	;;
+	w) word_size=${OPTARG}
+	;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+now=$(date)
+timestamp=$(date +%s)
+printf "$now \n"
+printf "$timestamp (temp)\n"
+printf "Subject file %s\n" "$subject"
+printf "Query file %s\n" "$query"
+printf "Output file %s\n" "$output"
+printf "perc_identity file %s\n" "$perc_identity"
+printf "word_size %s\n" "$word_size"
+tempfa="$(basename subject)${timestamp}"
+printf "temp_file %s\n" "$tempfa"
+#cat $subject
+
+#cp $subject $output
+echo -n "" > "$output"
+
+awk '(NR-1)%4<2'  $subject | sed -e "s/^@/>/" > $tempfa
+
+blast_patterns=($(blastn -query $query -subject $tempfa -perc_identity $perc_identity -word_size $word_size -outfmt 6 | awk '{print $2}'))
+for row in "${blast_patterns[@]}"
+do
+   :
+   blast_pattern_assoc[$row]=""
+done
+
+copy=1
+init=1
+while read line; do
+		# reading each line
+	#echo $line
+	if [[ $line == @* ]]
+	then
+		#echo $line
+		id=$(echo $line|awk '{print $1}')
+		id="${id:1}"		
+		if [[ -v "blast_pattern_assoc[$id]" ]] ; then
+			#echo "SET"
+			#echo $id
+			copy=0
+			init=0
+		fi
+	fi
+	if [ $copy -eq 1 ]
+	then
+		echo "$line" >> "$output"
+	else #copyy eq 0 (false)
+		if [ $init -lt 4 ]
+		then
+			#echo "SKIP"
+			init=$((init+1))
+		else
+			copy=1
+			init=0
+		fi		
+	fi
+	
+done < $subject
+
+
+#lines=$(echo $blast_patterns | tr "\n" "\n")
+#for pattern in $lines
+#do
+#	#echo $pattern
+#	sed -i '/@$pattern/,+4 d' $output
+#done
+
+now=$(date)
+rm $tempfa
+printf "temp_file %s removed\n" "$tempfa"
+printf "Finish $now \n"
